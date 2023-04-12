6802ef40ca44e07632d06d138c9e54de9f425c26
Anthony <anthony.wetzstein@gmail.com>
2023-04-12 09:44:50 +0200

    Checking if imput is number

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-const textFormQuerySelector = document.querySelector("#converter")
-const realTime = setInterval(converstionInFrancs, 5000)
-let auth = true
-
-function converstionInFrancs()
+const textFormQuerySelector = document.querySelector("#converter")                      //**********
+const realTime = setInterval(converstionInFrancs, 5000)                                 //*Conversion in real time so we need an interval
+let auth = true                                                                         //*Weed need some kind of switch or else our output will be divided 
+                                                                                        //*infinitly until it reaches 0
+let moneytoConvert = textFormQuerySelector.value    
+let moneyConverted
+function converstionInFrancs()                                                          //********/
 {
     if (auth == true)
     {
-        let moneytoConvert = textFormQuerySelector.value
-        console.log(moneytoConvert)
-        let moneyConverted = (moneytoConvert / 6.55957)
-        console.log(moneyConverted)
-        moneyConverted = roundTo(moneyConverted,2)
-        textFormQuerySelector.value = moneyConverted
-        console.log("work?")        
-        auth = false
+        moneytoConvert = textFormQuerySelector.value           
+        if (isNaN(moneytoConvert) == true || moneytoConvert == "")                                                      //We check if user did put something
+        {
+
+        }
+        else
+        {
+
+            console.log(moneytoConvert)
+            moneyConverted = (moneytoConvert / 6.55957)
+            console.log(moneyConverted)
+            moneyConverted = roundTo(moneyConverted,2)
+            textFormQuerySelector.value = moneyConverted
+            console.log("work?")        
+            auth = false            
+        }
     }
 
 }
-function roundTo(n, place) {    
+function roundTo(n, place)                                                            //Function used for rounding
+{    
     return +(Math.round(n + "e+" + place) + "e-" + place);
 }
